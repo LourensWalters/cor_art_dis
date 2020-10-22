c49d273dc3eef1e56a060c453865ee68cc87e97d
Lourens Walters <lourens.walters@gmail.com>
2020-10-22 11:34:04 +0100

    Initial commit.

---
+++ cor_art_dis/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The main objective of the analysis was to explore the Cleveland "Coronary Artery Disease" dataset (Detrano et al., 1989)(Patel, 2020) by applying various ML techniques to better understand the information contained in the dataset, and to document any findings.',
+    author='Lourens O Walters',
+    license='MIT',
+)
